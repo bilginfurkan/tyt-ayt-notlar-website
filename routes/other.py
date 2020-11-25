2ad0c7dabc226b5802df3c30ad65b53631d46c09
Furkan Bilgin <gitea@fake.local>
2020-11-25 18:38:55 +0300

    Added response codes to errors

--- routes/other.py
+++ routes/other.py
@@ -4,14 +4,14 @@ from flask import current_app as app
 
 @app.errorhandler(404)
 def error_404(exc):
-    return render_template("error.html", content="Bu sayfa bulunamadı.")
+    return render_template("error.html", content="Bu sayfa bulunamadı."), 404
 
 
 @app.errorhandler(403)
 def error_404(exc):
-    return render_template("error.html", content="Bu sayfaya girmeniz mümkün değil :'(")
+    return render_template("error.html", content="Bu sayfaya girmeniz mümkün değil :'("), 403
 
 
 @app.errorhandler(500)
 def error_500(exc):
-    return render_template("error.html", content="Sunucu hatası! Bir süre sonra tekrar deneyin.")
+    return render_template("error.html", content="Sunucu hatası! Bir süre sonra tekrar deneyin."), 500
