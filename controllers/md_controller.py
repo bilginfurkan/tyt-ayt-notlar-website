e766b4ef5a337878607a53660d187e4972524d0c
Furkan Bilgin <furkan.bilgin@outlook.com>
2021-04-06 17:57:04 +0300

    Client-side rendering

--- controllers/md_controller.py
+++ controllers/md_controller.py
@@ -3,7 +3,7 @@ import re
 from markdown2 import markdown
 
 def process_md(context):
-    context = markdown(context, extras=["break-on-newline"])
+    #context = markdown(context, extras=["break-on-newline"])
     context = re.sub(r"!\[\[(.+)\]\]", r"<img src='/medya/\1'>", context)
 
     return context
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,7 @@
-// empty lol
+if (typeof markdownData !== 'undefined') {
+    let md = window.markdownit();
+    let render = md.render(markdownData);
+    render = render.replace(/!\[\[(.+)\]\]/g, "<img src='/medya/$1'>")
+
+    $('#content').html(render);
+}
